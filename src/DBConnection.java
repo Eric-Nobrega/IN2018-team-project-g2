72719bb64c9e8cc274740276910be6685a27095a
Eric Nobrega <ericnobrega02@gmail.com>
2023-03-23 17:36:51 +0000

    modified return type
    if the db connection is successful, the class will return a live connection that can be interacted with, else return null and log an error

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,8 +1,9 @@
 import java.sql.*;
 
 public class DBConnection {
-    public static void main(String[] args) {
+    public static Connection connectDB() {
 
+        // database url
         String url = "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g02";
 
         // admin user, with DDL statement rights
@@ -11,12 +12,13 @@ public class DBConnection {
 
         try {
             Connection conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Connection successful!");
-            conn.close();
+            System.out.println("Database Connection Successful.");
+            return conn;
         } catch (SQLException e) {
-            System.out.println("Connection failed.");
+            System.out.println("Database Connection failed.");
             e.printStackTrace();
         }
+        return null;
     }
 }
 
