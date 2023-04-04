3c249fdb6cd840bf2947820f0cd41c8c9f83ad10
Eric Nobrega <ericnobrega02@gmail.com>
2023-04-04 12:20:05 +0100

    added navigation buttons (no logic yet)

--- src/pages/FlightsPage.java
+++ src/pages/FlightsPage.java
@@ -6,8 +6,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class FlightsPage extends BorderPane {
@@ -29,6 +29,21 @@ public class FlightsPage extends BorderPane {
 
         BorderPane.setMargin(welcomeLabel, new Insets(45));
         BorderPane.setMargin(centerBox, new Insets(50, 10, 10, 10));
+
+        // Create the HBox for the buttons and add them
+        HBox buttonBox = new HBox();
+        buttonBox.setSpacing(10);
+        Button button1 = new Button("Continue To Payment");
+        Button button2 = new Button("Return To Home");
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        buttonBox.getChildren().addAll(button1, spacer, button2);
+
+        // Add margin to the bottom and left of the buttonBox
+        BorderPane.setMargin(buttonBox, new Insets(0, 10, 10, 10));
+
+        // Add the HBox to the bottom of the BorderPane
+        this.setBottom(buttonBox);
 
         this.setTop(welcomeLabel);
         this.setCenter(centerBox);
